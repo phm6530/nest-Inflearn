3396f673f5c0c2c8c0903d7488fc82a363e53565
phm6530 <squirrel309@naver.com>
2024-10-05 18:29:59 +0900

    인프런 nest 강의

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
-import { BoardModule } from './board/board.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [BoardModule],
+  imports: [PostsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
